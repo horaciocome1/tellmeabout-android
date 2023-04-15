5d8a22117e01f65b84f365aee5db99c0a491940f
Horácio Comé <horacio.come@wit-software.com>
2023-04-16 00:34:33 +0100

    adds many things

--- app/src/main/java/io/github/horaciocome1/factsai/ui/screens/FactsScreen.kt
+++ app/src/main/java/io/github/horaciocome1/factsai/ui/screens/FactsScreen.kt
@@ -4,10 +4,13 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.CircularProgressIndicator
@@ -66,6 +69,7 @@ fun FactsScreen(
                     text = facts[currentPage],
                     modifier = Modifier
                         .fillMaxSize(0.9f)
+                        .windowInsetsPadding(WindowInsets.systemBars)
                         .clip(
                             shape = MaterialTheme.shapes.large,
                         )
